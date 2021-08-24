82b5f229320947638f91d3c11b78e86c33f45460
Adrian Gette <adrian.gette@icloud.com>
2021-08-24 15:58:06 -0300

    Inicialize chat app project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine, ChatFeed } from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="469e36ec-2757-444d-842a-d0c44a5ef28d"
+      userName="AdrianGette"
+      userSecret="32967811"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 

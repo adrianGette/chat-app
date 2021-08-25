2200a00f1b86cac795eb52173199913ae41ec005
Adrian Gette <adrian.gette@icloud.com>
2021-08-25 00:03:58 -0300

    Finish NessageForm

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,15 +1,30 @@
 import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
 
     const [ value, setValue ] = useState('');
+    const { chatID, creds } = props;
 
-    const handleSubmit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
+        const text = value.trim();
+
+        if(text.length > 0) sendMessage(creds, chatID, {text});
+
+        setValue('');
     }
 
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
 
+        isTyping(props, chatID);
+    }
+
+    const handleUpload = (event) => {
+        sendMessage(creds, chatID, { files: event.target.files, text: '' })
     }
 
     return (
@@ -21,6 +36,21 @@ const MessageForm = () => {
                 onChange={handleChange}
                 onSubmit={handleSubmit}
             />
+            <label htmlFor="upload-button">
+                <span className="image-button">
+                    <PictureOutlined className="picture-icon" />
+                </span>
+            </label>
+            <input 
+                type="file"
+                multiple={false}
+                id="upload-button"
+                style={{ display: 'none' }}
+                onChange={handleUpload}
+            />
+            <button type="submit" className="send-button">
+                <SendOutlined className="send-icon" />
+            </button>
         </form>
     );
 }
